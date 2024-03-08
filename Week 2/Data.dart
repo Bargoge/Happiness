c5e67a366a13826a319f8b13c84e18eb911865f4
moureebargoge@gmail.com <moureenbargoge@gmail.com>
2024-03-08 20:48:19 +0300

    dart datatype

---
+++ Week 2/Data.dart
@@ -0,0 +1,27 @@
+// Dart program demonstrating data types
+
+void main() {
+  // Integers
+  int integerNumber = 10;
+  print('Integer: $integerNumber');
+
+  // Doubles
+  double doubleNumber = 3.14;
+  print('Double: $doubleNumber');
+
+  // Strings
+  String message = 'Hello, Dart!';
+  print('String: $message');
+
+  // Lists
+  List<int> numbersList = [1, 2, 3, 4, 5];
+  print('List: $numbersList');
+
+  // Maps
+  Map<String, dynamic> person = {
+    'name': 'John',
+    'age': 30,
+    'isEmployed': true,
+  };
+  print('Map: $person');
+}
